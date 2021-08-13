55a86737b13516c862aaf59f193d07fd41acd4fe
Richard Hansen <rhansen@rhansen.org>
2021-08-13 02:25:57 -0400

    WIP: webpack

--- src/node/hooks/express/specialpages.js
+++ src/node/hooks/express/specialpages.js
@@ -6,11 +6,13 @@ const fs = require('fs');
 const fsp = fs.promises;
 const toolbar = require('../../utils/toolbar');
 const hooks = require('../../../static/js/pluginfw/hooks');
+const plugins = require('../../../static/js/pluginfw/plugin_defs');
 const settings = require('../../utils/Settings');
 const util = require('util');
 const webaccess = require('./webaccess');
+const webpack = require('webpack');
 
-exports.expressCreateServer = (hookName, args, cb) => {
+exports.expressCreateServer = async (hookName, args) => {
   // expose current stats
   args.app.get('/stats', (req, res) => {
     res.json(require('../../stats').toJSON());
@@ -46,6 +48,51 @@ exports.expressCreateServer = (hookName, args, cb) => {
     });
   });
 
+  await fsp.mkdir(path.join(settings.root, 'var/js'), {recursive: true});
+  await fsp.writeFile(
+      path.join(settings.root, 'var/js/padbootstrap.js'),
+      eejs.require('ep_etherpad-lite/templates/padbootstrap.js', {
+        pluginModules: (() => {
+          const pluginModules = new Set();
+          for (const part of plugins.parts) {
+            for (const [, hookFnName] of Object.entries(part.client_hooks || {})) {
+              pluginModules.add(hookFnName.split(':')[0]);
+            }
+          }
+          return [...pluginModules];
+        })(),
+        settings,
+      }));
+
+  console.log('Packaging client-side JavaScript...');
+  const compiler = webpack({
+    context: settings.root,
+    devtool: 'source-map',
+    entry: './var/js/padbootstrap.js',
+    mode: process.env.NODE_ENV || 'development',
+    module: {
+      parser: {
+        javascript: {
+          commonjsMagicComments: true,
+        },
+      },
+    },
+    output: {
+      path: path.join(settings.root, 'var/js'),
+      filename: '[name]-[contenthash].js',
+    },
+  });
+  const stats = await util.promisify(compiler.run.bind(compiler))();
+  console.log(`webpack stats:\n${stats}`);
+  const mainName = stats.toJson('minimal').assetsByChunkName.main;
+
+  args.app.get(`/${mainName}`, (req, res, next) => {
+    res.sendFile(path.join(settings.root, `var/js/${mainName}`));
+  });
+  args.app.get(`/${mainName}.map`, (req, res, next) => {
+    res.sendFile(path.join(settings.root, `var/js/${mainName}.map`));
+  });
+
   // serve pad.html under /p
   args.app.get('/p/:pad', (req, res, next) => {
     // The below might break for pads being rewritten
@@ -62,6 +109,7 @@ exports.expressCreateServer = (hookName, args, cb) => {
       req,
       toolbar,
       isReadOnly,
+      entrypoint: `../${mainName}`,
     }));
   });
 
@@ -97,6 +145,4 @@ exports.expressCreateServer = (hookName, args, cb) => {
       next();
     })().catch((err) => next(err || new Error(err)));
   });
-
-  return cb();
 };
--- src/static/js/pluginfw/client_plugins.js
+++ src/static/js/pluginfw/client_plugins.js
@@ -7,49 +7,34 @@ exports.baseURL = '';
 
 exports.ensure = (cb) => !defs.loaded ? exports.update(cb) : cb();
 
-exports.update = (cb) => {
-  // It appears that this response (see #620) may interrupt the current thread
-  // of execution on Firefox. This schedules the response in the run-loop,
-  // which appears to fix the issue.
-  const callback = () => setTimeout(cb, 0);
-
-  jQuery.getJSON(
-      `${exports.baseURL}pluginfw/plugin-definitions.json?v=${clientVars.randomVersionString}`
-  ).done((data) => {
-    defs.plugins = data.plugins;
-    defs.parts = data.parts;
-    defs.hooks = pluginUtils.extractHooks(defs.parts, 'client_hooks');
-    defs.loaded = true;
-    callback();
-  }).fail((err) => {
-    console.error(`Failed to load plugin-definitions: ${err}`);
-    callback();
-  });
+exports.update = async (modules) => {
+  const data = await jQuery.getJSON(
+      `${exports.baseURL}pluginfw/plugin-definitions.json?v=${clientVars.randomVersionString}`);
+  defs.plugins = data.plugins;
+  defs.parts = data.parts;
+  defs.hooks = pluginUtils.extractHooks(defs.parts, 'client_hooks', null, modules);
+  defs.loaded = true;
 };
 
 const adoptPluginsFromAncestorsOf = (frame) => {
   // Bind plugins with parent;
-  let parentRequire = null;
+  let ancestorPluginDefs = null;
   try {
-    while ((frame = frame.parent)) {
-      if (typeof (frame.require) !== 'undefined') {
-        parentRequire = frame.require;
-        break;
-      }
+    while (frame !== frame.parent && (frame = frame.parent) && ancestorPluginDefs == null) {
+      ancestorPluginDefs = frame.pluginDefs;
     }
   } catch (error) {
     // Silence (this can only be a XDomain issue).
     console.error(error);
   }
 
-  if (!parentRequire) throw new Error('Parent plugins could not be found.');
+  if (!ancestorPluginDefs) throw new Error('Parent plugins could not be found.');
 
-  const ancestorPluginDefs = parentRequire('ep_etherpad-lite/static/js/pluginfw/plugin_defs');
   defs.hooks = ancestorPluginDefs.hooks;
   defs.loaded = ancestorPluginDefs.loaded;
   defs.parts = ancestorPluginDefs.parts;
   defs.plugins = ancestorPluginDefs.plugins;
-  const ancestorPlugins = parentRequire('ep_etherpad-lite/static/js/pluginfw/client_plugins');
+  const ancestorPlugins = frame.plugins;
   exports.baseURL = ancestorPlugins.baseURL;
   exports.ensure = ancestorPlugins.ensure;
   exports.update = ancestorPlugins.update;
